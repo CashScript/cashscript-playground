087d3f1e133d28bafd7b17e6de8e045622bded66
Mathieu Geukens <mr-zwets@protonmail.com>
2023-03-30 11:54:34 +0200

    fix font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import App from '../components/App';
 import Head from 'next/head';
+// pages/_app.js
+import { Roboto_Mono } from 'next/font/google'
+
+// If loading a variable font, you don't need to specify the font weight
+const roboto_Mono = Roboto_Mono({ subsets: ['latin'] })
 
 const IndexPage = () => {
   return (
-    <>
+    <div className={roboto_Mono.className}>
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
         <title>Next-Cashscript-Playground </title>
       </Head>
-      <App />
-    </>
+      <App/>
+    </div>
   );
 };
 
