ce5c690dbdb03a58fbec45bc895df0f2748cf6f4
Mathieu Geukens <mr-zwets@protonmail.com>
2024-05-24 12:33:21 +0200

    electrum subscription contract balance

--- src/components/ContractInfo.tsx
+++ src/components/ContractInfo.tsx
@@ -3,6 +3,7 @@ import { Artifact, Contract, Network, Utxo } from 'cashscript'
 import { ColumnFlex, Wallet } from './shared'
 import ContractCreation from './ContractCreation'
 import ContractFunctions from './ContractFunctions'
+import { ElectrumClient, ElectrumTransport } from 'electrum-cash'
 
 interface Props {
   artifact?: Artifact
@@ -17,8 +18,34 @@ const ContractInfo: React.FC<Props> = ({ artifact, network, setNetwork, setShowW
   const [contract, setContract] = useState<Contract | undefined>(undefined)
   const [balance, setBalance] = useState<bigint | undefined>(undefined)
   const [utxos, setUtxos] = useState<Utxo[] | undefined>([])
+  const [electrumClient, setElectrumClient] = useState<ElectrumClient | undefined>(undefined)
 
   useEffect(() => setContract(undefined), [artifact])
+
+  async function initElectrumSubscription(){
+    if(electrumClient) electrumClient?.disconnect()
+    if(!contract?.address) return
+
+    // connect to ElectrumClient for subscription
+    let electrumServerName: string= ""
+    if(network == "mainnet") electrumServerName = 'bch.imaginary.cash'
+    if(network == "chipnet")electrumServerName = 'chipnet.imaginary.cash'
+    if(network == "testnet4") electrumServerName = 'testnet4.imaginary.cash'
+    if(!electrumServerName) return // no imaginary server for  testnet3
+    
+    const newElectrumClient = new ElectrumClient('Electrum client example', '1.4.1', electrumServerName, ElectrumTransport.WSS.Port, ElectrumTransport.WSS.Scheme)
+    await newElectrumClient?.connect();
+    // subscribe to contract address
+    const refetchContractBalance = async(data:any) => {
+      if(data) updateUtxosContract()
+    }
+    await newElectrumClient?.subscribe(refetchContractBalance, 'blockchain.address.subscribe', contract.address);
+    setElectrumClient(newElectrumClient)
+  }
+
+  useEffect(() => {
+    initElectrumSubscription()
+  }, [contract])
 
   async function updateUtxosContract () {
     if (!contract) return
