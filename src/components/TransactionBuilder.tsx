c31a68943d7f9b10c188e1f9d43527cf4b149815
Mathieu Geukens <mr-zwets@protonmail.com>
2025-03-06 10:24:22 +0100

    add inputFields

--- src/components/TransactionBuilder.tsx
+++ src/components/TransactionBuilder.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
-import { AbiFunction, NetworkProvider, Recipient } from 'cashscript'
-import { Wallet, ContractInfo, NamedUtxo } from './shared'
+import { AbiFunction, NetworkProvider, Recipient, SignatureTemplate, TransactionBuilder, Unlocker } from 'cashscript'
+import { Wallet, ContractInfo, NamedUtxo, ExplorerString } from './shared'
 import { Button, Card, Form, InputGroup } from 'react-bootstrap'
 
 interface Props {
@@ -21,6 +21,134 @@ const TransactionBuilderPage: React.FC<Props> = ({ provider, wallets, contracts,
   const [outputHasFT, setOutputHasFT] = useState<boolean[]>([])
   const [outputHasNFT, setOutputHasNFT] = useState<boolean[]>([])
   const [namedUtxoList, setNamedUtxoList] = useState<NamedUtxo[]>([])
+
+  // setNamedUtxoList on initial render and on contractInfo updates 
+  useEffect(() => {
+    async function updateUtxos() {
+      let newNamedUtxoList = [] as NamedUtxo[]
+      const walletUtxos = wallets.map(wallet => wallet?.utxos ?? [])
+      for (let i = 0; i < (walletUtxos?.length ?? 0); i++) {
+        const utxosWallet = walletUtxos?.[i];
+        if(!utxosWallet) continue
+        const namedUtxosWallet: NamedUtxo[] = utxosWallet.map((utxo, index) => (
+          { ...utxo, name: `${wallets[i].walletName} UTXO ${index}`, isP2pkh: true, walletIndex: i }
+        ))
+        newNamedUtxoList = namedUtxosWallet
+      }
+      setNamedUtxoList(newNamedUtxoList);
+    }
+    updateUtxos()
+  }, [wallets])
+
+  function selectInput(i: number, inputIndex: string) {
+    const inputsCopy = [...inputs];
+    // if no input is selected in select form
+    if (isNaN(Number(inputIndex))) inputsCopy[i] = { txid: '', vout: 0, satoshis: 0n, name: ``, isP2pkh: false }
+    else {
+      inputsCopy[i] = namedUtxoList[Number(inputIndex)];
+    }
+    setInputs(inputsCopy);
+  }
+
+  const inputFields = inputs.map((input, index) => (
+    <div  key={`input-${index}`}>
+      {`Input #${index}`}
+      <div>
+        <Form.Control size="sm" id="artifact-selector" style={{width:"200px", display:"inline-block"}}
+          as="select"
+          onChange={(event) => {}}
+        >
+          <option>--- select ---</option> 
+          <option>Wallet input</option>
+          <option>Contract input</option>
+        </Form.Control>
+        <Form.Control
+          onChange={event => selectInput(index, event.target.value)}
+          as="select"
+          style={{ width: 'calc(50% - 210px)', display: 'inline-block', marginLeft: '10px' }}
+          size="sm"
+        >
+          <option className="text-center" key='Nan' value={`NaN`}>select UTXO</option>
+          {namedUtxoList.map((utxo, inputIndex) => (
+            <option className="text-center" key={`${inputIndex + utxo.name}`} value={`${inputIndex}`}> {utxo.name} </option>
+          ))}
+        </Form.Control>
+      </div>
+    </div>
+  ))
+
+  const tokenFields = (index: number) => (
+    <>
+    <InputGroup key={`output-${index}-tokens`}>
+      <Form.Control size="sm"
+        placeholder="Token Category"
+        aria-label="Token Category"
+        onChange={(event) => {
+          const outputsCopy = [...outputs]
+          const output = outputsCopy[index]
+          const amount = output.token?.amount || 0n
+          const category = event.target.value
+          output.token = {...output.token, amount, category}
+          outputsCopy[index] = output
+          setOutputs(outputsCopy)
+        }}
+      />
+      <Form.Control size="sm"
+        placeholder="Amount Fungible Tokens"
+        aria-label="Amount Fungible Tokens"
+        onChange={(event) => {
+          const outputsCopy = [...outputs]
+          const output = outputsCopy[index]
+          const category = output.token?.category || ""
+          const amount = BigInt(event.target.value)
+          output.token = {...output.token, amount, category}
+          outputsCopy[index] = output
+          setOutputs(outputsCopy)
+        }}
+      />
+    </InputGroup>
+    {outputHasNFT[index] ? (
+        <div>
+          <InputGroup key={`output-${index}-NFT`}>
+            <Form.Control size="sm"
+              placeholder="Token Commitment"
+              aria-label="Token Commitment"
+              onChange={(event) => {
+                const outputsCopy = [...outputs]
+                const output = outputsCopy[index]
+                const capability = output.token?.nft?.capability || "none"
+                const commitment = event.target.value
+                if(!output.token) output.token = {amount: 0n , category:""}
+                output.token.nft = {capability, commitment}
+                outputsCopy[index] = output
+                setOutputs(outputsCopy)
+              }}
+            />
+            <Form.Control size="sm" id="capability-selector"
+              key={`output-${index}-capability-selector`}
+              as="select"
+              onChange={(event) => {
+                const outputsCopy = [...outputs]
+                const output = outputsCopy[index]
+                const commitment = output.token?.nft?.commitment || ""
+                const capability= event.target.value
+                if(capability != "none" && capability != "minting" && capability != "mutable") return
+                if(!output.token) output.token = {amount: 0n , category:""}
+                output.token.nft = {capability, commitment}
+                outputsCopy[index] = output
+                setOutputs(outputsCopy)
+              }}
+            >
+              <option>Select Capability</option>
+              <option value={"none"}>none</option>
+              <option value={"minting"}>minting</option>
+              <option value={"mutable"}>mutable</option>
+            </Form.Control>
+          </InputGroup>
+        </div>)
+        : null}
+    </>
+  )
 
   const outputFields = outputs.map((output, index) => (
     <div  key={`output-${index}`}>
@@ -87,79 +215,6 @@ const TransactionBuilderPage: React.FC<Props> = ({ provider, wallets, contracts,
     </div>
   ))
 
-    const tokenFields = (index: number) => (
-      <>
-      <InputGroup key={`output-${index}-tokens`}>
-        <Form.Control size="sm"
-          placeholder="Token Category"
-          aria-label="Token Category"
-          onChange={(event) => {
-            const outputsCopy = [...outputs]
-            const output = outputsCopy[index]
-            const amount = output.token?.amount || 0n
-            const category = event.target.value
-            output.token = {...output.token, amount, category}
-            outputsCopy[index] = output
-            setOutputs(outputsCopy)
-          }}
-        />
-        <Form.Control size="sm"
-          placeholder="Amount Fungible Tokens"
-          aria-label="Amount Fungible Tokens"
-          onChange={(event) => {
-            const outputsCopy = [...outputs]
-            const output = outputsCopy[index]
-            const category = output.token?.category || ""
-            const amount = BigInt(event.target.value)
-            output.token = {...output.token, amount, category}
-            outputsCopy[index] = output
-            setOutputs(outputsCopy)
-          }}
-        />
-      </InputGroup>
-      {outputHasNFT[index] ? (
-          <div>
-            <InputGroup key={`output-${index}-NFT`}>
-              <Form.Control size="sm"
-                placeholder="Token Commitment"
-                aria-label="Token Commitment"
-                onChange={(event) => {
-                  const outputsCopy = [...outputs]
-                  const output = outputsCopy[index]
-                  const capability = output.token?.nft?.capability || "none"
-                  const commitment = event.target.value
-                  if(!output.token) output.token = {amount: 0n , category:""}
-                  output.token.nft = {capability, commitment}
-                  outputsCopy[index] = output
-                  setOutputs(outputsCopy)
-                }}
-              />
-              <Form.Control size="sm" id="capability-selector"
-                key={`output-${index}-capability-selector`}
-                as="select"
-                onChange={(event) => {
-                  const outputsCopy = [...outputs]
-                  const output = outputsCopy[index]
-                  const commitment = output.token?.nft?.commitment || ""
-                  const capability= event.target.value
-                  if(capability != "none" && capability != "minting" && capability != "mutable") return
-                  if(!output.token) output.token = {amount: 0n , category:""}
-                  output.token.nft = {capability, commitment}
-                  outputsCopy[index] = output
-                  setOutputs(outputsCopy)
-                }}
-              >
-                <option>Select Capability</option>
-                <option value={"none"}>none</option>
-                <option value={"minting"}>minting</option>
-                <option value={"mutable"}>mutable</option>
-              </Form.Control>
-            </InputGroup>
-          </div>)
-          : null}
-      </>
-    )
-
   function addOutput() {
     const outputsCopy = [...outputs]
     outputsCopy.push({ to: '', amount: 0n })
@@ -180,6 +235,52 @@ const TransactionBuilderPage: React.FC<Props> = ({ provider, wallets, contracts,
     const inputsCopy = [...inputs]
     inputsCopy.splice(-1)
     setInputs(inputsCopy)
+  }
+
+  async function sendTransaction() {  
+    // try to send transaction and alert result
+    try {
+      // start constructing transaction
+      const transaction = new TransactionBuilder({provider})
+
+      // add inputs to transaction in correct order
+      for(const input of inputs){
+        if(input.isP2pkh){
+          const walletIndex = input.walletIndex as number
+          const sigTemplate = new SignatureTemplate(wallets[walletIndex].privKey)
+          transaction.addInput(input, sigTemplate.unlockP2PKH())
+        } else {
+          const inputUnlocker = {} as Unlocker
+          transaction.addInput(input, inputUnlocker)
+        }
+      }
+
+      transaction.addOutputs(outputs)
+      if(enableLocktime) transaction.setLocktime(Number(locktime))
+
+      // check for mocknet
+      if(provider.network == "mocknet"){
+        try{
+          await transaction.debug()
+          alert(`Transaction evalution passed! see Bitauth IDE link in console`)
+        } catch(error) {
+          const errorMessage = typeof error == "string" ? error : (error as Error)?.message
+          const cashscriptError = errorMessage.split("Bitauth")[0]
+          console.error(errorMessage)
+          alert(`Transaction evalution failed with the following message: \n\n${cashscriptError} See Bitauth IDE link in console`)
+        }
+        
+        console.log(`Transaction evalution passed! Bitauth IDE link: ${await transaction.bitauthUri()}`)
+      } else {
+        const { txid } = await transaction.send()
+        alert(`Transaction successfully sent! see explorer link in console`)
+        console.log(`Transaction successfully sent: ${ExplorerString[provider.network]}/tx/${txid}`)
+      }
+      // TODO: update utxos if using real network
+    } catch (e: any) {
+      alert(e.message)
+      console.error(e.message)
+    }
   }
 
   return (
@@ -223,6 +324,7 @@ const TransactionBuilderPage: React.FC<Props> = ({ provider, wallets, contracts,
             <Button variant="outline-secondary" size="sm" onClick={addInput}>+</Button>
           </Card.Header>
           <Card.Body>
+            {inputFields}
           </Card.Body>
       </Card>
 
@@ -237,7 +339,7 @@ const TransactionBuilderPage: React.FC<Props> = ({ provider, wallets, contracts,
           </Card.Body>
       </Card>
 
-      <Button variant="secondary" style={{ display: "block" }} size="sm" onClick={() => {}}>
+      <Button variant="secondary" style={{ display: "block" }} size="sm" onClick={() => sendTransaction}>
         { provider.network === "mocknet" ? "Evaluate" : "Send" }
       </Button>
     </div>
