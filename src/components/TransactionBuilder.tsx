68a8be942c6ff9ae6630e7eb4b267772b13982c4
Mathieu Geukens <mr-zwets@protonmail.com>
2025-03-06 08:24:50 +0100

    start multi-contract rework

--- src/components/TransactionBuilder.tsx
+++ src/components/TransactionBuilder.tsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react'
-import { AbiFunction, NetworkProvider } from 'cashscript'
+import { AbiFunction, NetworkProvider, Recipient } from 'cashscript'
 import ContractFunction from './ContractFunction';
-import { Wallet, ContractInfo } from './shared'
-import { Form } from 'react-bootstrap'
+import { Wallet, ContractInfo, NamedUtxo } from './shared'
+import { Button, Card, Form } from 'react-bootstrap'
 
 interface Props {
   provider: NetworkProvider
@@ -13,55 +13,33 @@ interface Props {
 
 const TransactionBuilder: React.FC<Props> = ({ provider, wallets, contracts, updateUtxosContract }) => {
 
-  const [selectedContract, setSelectedContract] = useState<ContractInfo | undefined>(undefined);
-  const [selectedFunction, setSelectedFunction] = useState<AbiFunction | undefined>(undefined);
 
-  useEffect(() => {
-    const contractName = selectedContract?.contract.name
-    const updatedContract = contracts?.find(contractInfo => contractInfo.contract?.name === contractName)
-    if(updatedContract != selectedContract) setSelectedContract(updatedContract)
-  }, [contracts, wallets])
+  const [outputs, setOutputs] = useState<Recipient[]>([{ to: '', amount: 0n }])
+  // transaction inputs, not the same as abi.inputs
+  const [inputs, setInputs] = useState<NamedUtxo[]>([{ txid: '', vout: 0, satoshis: 0n, name: ``, isP2pkh: false }])
 
-  const contractSelector = (
-    <Form.Control size="sm" id="artifact-selector" style={{width:"350px", display:"inline-block"}}
-      as="select"
-      value={selectedContract?.contract.name ?? "select"}
-      onChange={(event) => {
-        const contractName = event.target?.value
-        const newSelectedContract = contracts?.find(contractInfo => contractInfo.contract?.name === contractName)
-        setSelectedContract(newSelectedContract)
-      }}
-    >
-      <option>--- select ---</option> 
-      {contracts?.map(contractInfo => (
-        <option key={contractInfo.contract.name} value={contractInfo.contract.name}>
-          {contractInfo.contract.name} {`(${contractInfo.contract.address.slice(0, 20)}...)`}
-        </option>
-      ))}
-    </Form.Control>
-  )
+  function addOutput() {
+    const outputsCopy = [...outputs]
+    outputsCopy.push({ to: '', amount: 0n })
+    setOutputs(outputsCopy)
+  }
+  function removeOutput() {
+    const outputsCopy = [...outputs]
+    outputsCopy.splice(-1)
+    setOutputs(outputsCopy)
+  }
 
-  const functionSelector = (
-    <Form.Control size="sm" id="artifact-selector" style={{width:"350px", display:"inline-block"}}
-      as="select"
-      value={selectedFunction?.name ?? "select"}
-      onChange={(event) => {
-        if(!selectedContract) return
-        const functionName = event.target?.value
-        const contractFunction = selectedContract.contract.artifact.abi.find(abiFunction =>
-          abiFunction.name == functionName
-        )
-        setSelectedFunction(contractFunction)
-      }}
-    >
-      <option>--- select ---</option> 
-      {selectedContract && selectedContract.contract.artifact.abi.map((abi) => (
-        <option key={abi.name} value={abi.name}>
-          {abi.name}
-        </option>
-      ))}
-    </Form.Control>
-  )
+  function addInput() {
+    const inputsCopy = [...inputs]
+    inputsCopy.push({ txid: '', vout: 0, satoshis: 0n, name: ``, isP2pkh: false })
+    setInputs(inputsCopy)
+  }
+  function removeInput() {
+    const inputsCopy = [...inputs]
+    inputsCopy.splice(-1)
+    setInputs(inputsCopy)
+  }
+
 
   return (
     <div style={{
@@ -77,23 +55,31 @@ const TransactionBuilder: React.FC<Props> = ({ provider, wallets, contracts, upd
       margin: "16px"
     }}>
       <h2>TransactionBuilder</h2>
-      <div style={{margin: "10px 0"}}>
-      {contracts?.length ?
-        <><span>Select Contract:</span> {contractSelector}</>
-        : <div>No contract initialised yet...</div>
-      }
-      </div>
+      <div style={{margin: "10px 0"}}> Add Inputs and Outputs to build the transaction:</div>
 
-      <div style={{margin: "10px 0"}}>
-      {selectedContract ?
-        <><span>Select Contract Function:</span> {functionSelector}</>
-        : null
-      }
-      </div>
-      {selectedContract !== undefined && selectedFunction !== undefined ?
-        <ContractFunction abi={selectedFunction} contractInfo={selectedContract} provider={provider} wallets={wallets} updateUtxosContract={updateUtxosContract} />
-        : null
-      }
+      <Card style={{ marginBottom: '10px' }}>
+          <Card.Header>Inputs{' '}
+            <Button variant="outline-secondary" size="sm" disabled={inputs.length <= 1} onClick={removeInput}>-</Button>
+              {' ' + inputs.length + ' '}
+            <Button variant="outline-secondary" size="sm" onClick={addInput}>+</Button>
+          </Card.Header>
+          <Card.Body>
+          </Card.Body>
+      </Card>
+
+      <Card style={{ marginBottom: '10px' }}>
+          <Card.Header>Outputs{' '}
+            <Button variant="outline-secondary" size="sm" disabled={outputs.length <= 1} onClick={removeOutput}>-</Button>
+              {' ' + outputs.length + ' '}
+            <Button variant="outline-secondary" size="sm" onClick={addOutput}>+</Button>
+          </Card.Header>
+          <Card.Body>
+          </Card.Body>
+      </Card>
+
+      <Button variant="secondary" style={{ display: "block" }} size="sm" onClick={() => {}}>
+        { provider.network === "mocknet" ? "Evaluate" : "Send" }
+      </Button>
     </div>
   )
 }
